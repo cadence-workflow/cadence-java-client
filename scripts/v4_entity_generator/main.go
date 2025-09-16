c23d8ddd0d4fb45c1029aab3aee5b9e802745d6b
Shijie Sheng <liouvetren@gmail.com>
2025-09-16 15:46:39 -0700

    add comments on the script
    Signed-off-by: Shijie Sheng <liouvetren@gmail.com>

--- scripts/v4_entity_generator/main.go
+++ scripts/v4_entity_generator/main.go
@@ -2,6 +2,10 @@ package main
 
 import "fmt"
 
+/*
+This script is used to generate the java entities from the thrift files. We will eventually remove this script and maintain entities without using thrift.
+*/
+
 func main() {
     if err := NewGenerator().Generate("../../src/main/idls/thrift/shared.thrift", "../../src/gen/java", "com.uber.cadence"); err != nil {
         panic(fmt.Sprintf("failed to generate: %v", err))
