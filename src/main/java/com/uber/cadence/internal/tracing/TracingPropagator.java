e34116124ca0546332fe04fe7be82ee1a50abb6e
Shijie Sheng <liouvetren@gmail.com>
2026-08-18 13:48:07 -0700

    feat(trace): add cadenceIsCron tag in the execute workflow span (#1071)
    What changed?
    
    Add cadenceIsCron tag on execute workflow span. (In Go, we add it in start workflow span)
    
    Why?
    cadence-workflow/cadence-go-client#1518
    
    How did you test it?
    
    Unit Test / Integration Test

--- src/main/java/com/uber/cadence/internal/tracing/TracingPropagator.java
+++ src/main/java/com/uber/cadence/internal/tracing/TracingPropagator.java
@@ -45,6 +45,7 @@ public class TracingPropagator {
   private static final String TAG_WORKFLOW_TYPE = "cadenceWorkflowType";
   private static final String TAG_WORKFLOW_RUN_ID = "cadenceRunID";
   private static final String TAG_ACTIVITY_TYPE = "cadenceActivityType";
+  private static final String TAG_IS_CRON = "cadenceIsCron";
 
   private final Tracer tracer;
 
@@ -70,6 +71,9 @@ public class TracingPropagator {
         .withTag(TAG_WORKFLOW_TYPE, context.getWorkflowType().getName())
         .withTag(TAG_WORKFLOW_ID, context.getWorkflowId())
         .withTag(TAG_WORKFLOW_RUN_ID, context.getRunId())
+        .withTag(
+            TAG_IS_CRON,
+            attributes.getCronSchedule() != null && !attributes.getCronSchedule().isEmpty())
         .start();
   }
 
